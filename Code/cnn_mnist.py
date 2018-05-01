55fd39f3cf268ac82fa56cce6d893c92db0f7c1a
Abhinav Das <abhinav.das1997@gmail.com>
2018-05-01 14:53:44 -0500

    using tensorboard

--- Code/cnn_mnist.py
+++ Code/cnn_mnist.py
@@ -98,7 +98,7 @@ def cnn_model_fn(features, labels, mode):
 
   # Calculate Loss (for both TRAIN and EVAL modes)
   loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
-
+  tf.summary.scalar('loss',loss)
   # Configure the Training Op (for TRAIN mode)
   if mode == tf.estimator.ModeKeys.TRAIN:
     optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
@@ -111,11 +111,22 @@ def cnn_model_fn(features, labels, mode):
   eval_metric_ops = {
       "accuracy": tf.metrics.accuracy(
           labels=labels, predictions=predictions["classes"])}
+
+  tf.summary.scalar('accuracy',tf.metrics.accuracy(
+          labels=labels, predictions=predictions["classes"]))
+
+
+  merged = tf.summary.merge_all()	
+
   return tf.estimator.EstimatorSpec(
       mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
 
 def main(unused_argv):
+  
+
+
+
   # Load training and eval data
   mnist = tf.contrib.learn.datasets.load_dataset("mnist")
   train_data = mnist.train.images  # Returns np.array
@@ -133,6 +144,7 @@ def main(unused_argv):
   logging_hook = tf.train.LoggingTensorHook(
       tensors=tensors_to_log, every_n_iter=50)
 
+
   # Train the model
   train_input_fn = tf.estimator.inputs.numpy_input_fn(
       x={"x": train_data},
@@ -140,10 +152,13 @@ def main(unused_argv):
       batch_size=100,
       num_epochs=None,
       shuffle=True)
+	
+  
   mnist_classifier.train(
       input_fn=train_input_fn,
-      steps=20000,
+      steps=1000,
       hooks=[logging_hook])
+  
 
   # Evaluate the model and print results
   eval_input_fn = tf.estimator.inputs.numpy_input_fn(
@@ -154,6 +169,9 @@ def main(unused_argv):
   eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
   print(eval_results)
 
+
+  writer = tf.summary.FileWriter("./test_1")
+  writer.add_summary(merged)
 
 if __name__ == "__main__":
 	tf.app.run()
---
+++ Code/cnn_mnist_improved.py
@@ -0,0 +1,98 @@
+#Design CNN with 2 convolutional layers, 2 pooling layers, and 2 fully connected layers with 1024 and 10 neurons
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+#Initialize params
+
+rate = 1e-3
+batch_size = 100 
+rounds = 2000
+dense_layer_neurons = 1024
+
+#Setup placeholders and reshape data
+
+x = tf.placeholder(tf.float32, shape=[None,784],name="x") #Pixels in training image
+y = tf.placeholder(tf.float32, shape=[None,10],name="labels")	#Label (one-hot)
+x_image = tf.reshape(x,[-1,28,28,1])
+
+#Create network
+
+conv1 = tf.layers.conv2d(
+      inputs=x_image,
+      filters=32,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
+
+pool1 = tf.nn.max_pool(conv1, ksize=[1,2,2,1], strides=[1,2,2,1], padding="SAME")
+
+conv2 = tf.layers.conv2d(
+      inputs=pool1,
+      filters=64,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
+
+pool2 = tf.nn.max_pool(conv2, ksize=[1,2,2,1], strides=[1,2,2,1], padding="SAME")
+flattened = tf.reshape(pool2, [-1,7*7*64])
+
+fc1 = tf.layers.dense(inputs=flattened, units=dense_layer_neurons, activation=tf.nn.relu)
+dropout = tf.layers.dropout(inputs=fc1, rate=0.4)
+logits = tf.layers.dense(inputs=dropout, units=10)
+
+#Training
+
+with tf.name_scope("cross_entropy"):
+	cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y)) #Cross-entropy for loss function
+
+tf.summary.scalar('cross_entropy',cross_entropy)
+
+with tf.name_scope("train"):
+	train_step = tf.train.GradientDescentOptimizer(learning_rate=rate).minimize(cross_entropy) #AdamOptimizer to train network
+
+
+with tf.name_scope("accuracy"):
+	correct_pred = tf.equal(tf.argmax(logits,1),tf.argmax(y,1)) #compute accuracy
+	accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+tf.summary.scalar('accuracy',accuracy)
+
+
+#Initialize variables
+init = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init)
+
+#Load Data:
+mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+train_data = mnist.train.images
+train_labels = np.array(mnist.train.labels, dtype=np.int32)
+
+#Setup Filewriter
+writer = tf.summary.FileWriter("./test_6")
+merged_summary = tf.summary.merge_all()	
+
+for i in range(rounds):
+	batch = mnist.train.next_batch(batch_size)
+	one_hot = np.zeros((batch[1].shape[0],10))
+	one_hot[np.arange(batch[1].shape[0]),batch[1]] = 1
+	if i%100 == 0:
+		s = sess.run(merged_summary, feed_dict={x: batch[0], y: one_hot})
+		writer.add_summary(s,i)	
+	if i%50 == 0:
+		[train_accuracy] = sess.run([accuracy], feed_dict={x: batch[0], y: one_hot})
+		print("step %d, training accuracy %g" % (i, train_accuracy))
+	sess.run(train_step,feed_dict={x: batch[0], y: one_hot})
+
+
+
+
+
+
